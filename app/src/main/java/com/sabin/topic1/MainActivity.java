2236df379dadd72c564dc29f3f72298105f5a900
sabing123 <sabingautam05@gmail.com>
2019-11-19 14:51:19 +0545

    Radio Button Topic Completed

--- app/src/main/java/com/sabin/topic1/MainActivity.java
+++ app/src/main/java/com/sabin/topic1/MainActivity.java
@@ -3,12 +3,57 @@ package com.sabin.topic1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etfirstnum, etsecondnum;
+    private Button btncalculate;
+    private RadioButton rdosum, rdosub, rdomulti, rdodivide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //binding
+
+        etfirstnum = findViewById(R.id.etfirstnum);
+        etsecondnum = findViewById(R.id.etsecondnum);
+        btncalculate = findViewById(R.id.btncalculate);
+        rdosum = findViewById(R.id.rdosum);
+        rdosub = findViewById(R.id.rdosubtract);
+        rdomulti = findViewById(R.id.rdomultiply);
+        rdodivide = findViewById(R.id.rdodivide);
+
+        btncalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int first, second, cal;
+                first = Integer.parseInt(etfirstnum.getText().toString());
+                second = Integer.parseInt(etsecondnum.getText().toString());
+
+                if (rdosum.isChecked()) {
+                    cal = first + second;
+                }
+                else if (rdosub.isChecked()){
+                    cal = first-second;
+                }
+                else if (rdomulti.isChecked()) {
+                    cal = first * second;
+                }
+                else {
+                    cal = first / second;
+                }
+                Toast.makeText(MainActivity.this, "Result is : " + cal, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
